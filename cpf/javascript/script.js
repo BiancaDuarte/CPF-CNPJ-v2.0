f2c0323adc78759ea792bfd7c3c7e2763702bb0a
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-04-28 17:33:17 -0300

    att

--- cpf/javascript/script.js
+++ cpf/javascript/script.js
@@ -11,30 +11,23 @@ $(document).ready(function(){
 
 function actions(){
 	$('#conferir').click(function(){
-		digitarCodigo();
 		buscarPosicao();
 		buscarPosicao2();
 		printMensagem();
 	});
 }
 
-function digitarCodigo(){
-	var cod = $('.numerosDigitados').val();
-}
-
 function buscarPosicao(){
 	var conta = 0;
+	var multiplicador = 10;
 	posicao = $('.numerosDigitados').val();
-	conta = conta + (posicao[0]*10); 
-	conta = conta + (posicao[1]*9);
-	conta = conta + (posicao[2]*8);
-	conta = conta + (posicao[3]*7);
-	conta = conta + (posicao[4]*6);
-	conta = conta + (posicao[5]*5);
-	conta = conta + (posicao[6]*4);
-	conta = conta + (posicao[7]*3);
-	conta = conta + (posicao[8]*2);
+	for(var i = 0; i<posicao.length; i++){
+		conta = conta + (posicao[i]*multiplicador);
+		multiplicador--;
+	}
+	console.log(conta);
 	conta = conta%11;
+
 
 	if(conta<2){
 		novoDigito = 0;
@@ -47,17 +40,13 @@ function buscarPosicao(){
 
 function buscarPosicao2(){
 	var conta2 = 0;
+	var multiplicador = 11;
 	posicao2 = $('.numerosDigitados').val();
-	conta2 = conta2 + (posicao2[0]*11); 
-	conta2 = conta2 + (posicao2[1]*10);
-	conta2 = conta2 + (posicao2[2]*9);
-	conta2 = conta2 + (posicao2[3]*8);
-	conta2 = conta2 + (posicao2[4]*7);
-	conta2 = conta2 + (posicao2[5]*6);
-	conta2 = conta2 + (posicao2[6]*5);
-	conta2 = conta2 + (posicao2[7]*4);
-	conta2 = conta2 + (posicao2[8]*3);
-	conta2 = conta2 + (posicao2[9]*2);
+	
+	for(var i = 0; i<posicao2.length; i++){
+		conta2 = conta2 + (posicao2[i]*multiplicador);
+		multiplicador--;
+	}
 	conta2 = conta2%11;
 
 	if(conta2<2){
@@ -65,23 +54,23 @@ function buscarPosicao2(){
 	}else{
 		novoDigito2 = 11 - conta2;
 	}
-
-	console.log(novoDigito2);
 }
 
 function printMensagem(){
-	if(posicao[9]==novoDigito){
-		if(posicao2[10]==novoDigito2){
-			$('.validou').show();
-			$('.naoValidou').hide();
-		}
-		else{
-			$('.naoValidou').show();
-			$('.validou').hide();
-		}
+	var flag = true;
+	var msg = 'CPF é ';
+	
+	if(posicao[9]!=novoDigito){
+		flag=false;
 	}
-	else{
-		$('.naoValidou').show();
-		$('.validou').hide();
+
+	if(posicao2[10]!=novoDigito2){
+		flag=false;
+	}
+	console.log(flag);
+	if(flag){
+		$('#msg').html(msg+'válido');
+	}else{
+		$('#msg').html(msg+'inválido');
 	}
 }
